253572567387f86391ff2ad68e4249059a7b8467
study_zxk <zxkr777@163.com>
2018-09-10 18:57:36 +0800

    add file

--- main.lua
+++ main.lua
@@ -4,6 +4,13 @@ gamestate= require (path.."hump/gamestate")
 mainFont = love.graphics.newFont("assets/xiheiti.ttf", 32)
 scaleX,scaleY = love.graphics.getWidth()/960, love.graphics.getHeight()/640
 function love.load()
+	local exists = love.filesystem.exists( "game.data" )
+	print(exists)
+	if not exists then
+		file = love.filesystem.newFile("game.data","w")
+		file:close()
+	end
+	--print(exists)
     gameState={}
     for _,name in ipairs(love.filesystem.getDirectoryItems(path.."scene")) do
         gameState[name:sub(1,-5)]=require(path.."scene."..name:sub(1,-5))
